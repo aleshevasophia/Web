7b5e7a4dc94e387e5eef7dfb38eb3ffde4ba5837
aleshevasophia <sophiaalesheva@mail.ru>
2022-03-23 15:00:06 +0300

    Работа вкладок (цикл)

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 var main = function () {
     "use strict";
-    var makeTabActive = function (tabNumber) {
-    var tabSelector = ".tabs a:nth-:child(" + tabNumber + ") span";
+    var tabNumber;
+    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
+    var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+    $(tabSelector).on("click", function () {
     $(".tabs span").removeClass("active");
-    $(tabSelector).addClass("active");
+    $(this).addClass("active");
+    return false;
+    });
+    }
     };
-    $(".tabs a:nth-child(1)").on("click", function () {
-    makeTabActive(1);
-    return false;
-    });
-    $(".tabs a:nth-child(2)").on("click", function () {
-    makeTabActive(2);
-    return false;
-    });
-    $(".tabs a:nth-child(3)").on("click", function () {
-    makeTabActive(3);
-    return false;
-    });
-    };
+    
