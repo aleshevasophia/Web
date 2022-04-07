a79f91f8f3fa9db95dd010fe354b192f8dd588e5
aleshevasophia <sophiaalesheva@mail.ru>
2022-04-07 12:39:47 +0300

    Создание функции organizationByTags

--- app.js
+++ app.js
@@ -1,99 +1,101 @@
-var main = function (toDoObjects) {
-    var toDos = toDoObjects.map(function(toDo) {
-        return toDo.description;
-    });
+var organizeByTags = function (toDoObjects) { 
 
-    var toDos = ["Купить продукты",
-                 "Обновить несколько новых задач",
-                 "Подготовиться к лекции в понедельник",
-                 "Ответить на письма нанимателей в LinkedIn",
-                 "Вывести Грейси на прогулку в парк",
-                 "Закончить писать книгу"];
+	console.log("organizeByTags called");
+	// создание пустого массива для тегов
+	var tags = [];
+	// перебираем все задачи toDos 
+	toDoObjects.forEach(function (toDo) {
+		// перебираем все теги для каждой задачи 
+		toDo.tags.forEach(function (tag) {
+			// убеждаемся, что этого тега еще нет в массиве
+			if (tags.indexOf(tag) === -1) { 
+				tags.push(tag);
+			}
+		});
+	}); 
+	console.log(tags);
 
-    $(".tabs a span").toArray().forEach(function (element) {
-        var $element = $(element);
-
-        $element.on("click", function () {
-            var $content,
-                $input,
-                $button,
-                i;
-            $(".tabs a span").removeClass("active");
-            $element.addClass("active");
-            $("main .content").empty();
-
-            if ($element.parent().is(":nth-child(1)")) {
-                $content = $("<ul>");
-                for (i = toDos.length-1; i >= 0; i--) {
-                    $content.append($("<li>").text(toDos[i]));
-                }
-            } else if ($element.parent().is(":nth-child(2)")) {
-                $content = $("<ul>");
-                toDos.forEach(function (todo) {
-                    $content.append($("<li>").text(todo));
-                });
-
-            } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("Щелчок на вкладке Теги");
-                var organizedByTag = [
-                    {
-                        "name": "покупки",
-                        "toDos": ["Купить продукты"]
-                        },
-                        {
-                        "name": "рутина",
-                        "toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
-                        },
-                        {
-                        "name": "писательство",
-                        "toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
-                        },
-                        {
-                        "name": "работа",
-                        "toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник","Ответить на электронные письма", "Закончить писать книгу"]
-                        },
-                        {
-                        "name": " преподавание",
-                        "toDos": ["Подготовиться к лекции в понедельник"]
-                        },
-                        {
-                        "name": "питомцы",
-                        "toDos": ["Вывести Грейси на прогулку в парк "]
-                        }
-                ]
-                organizedByTag.forEach(function (tag) {
-                    var $tagName=$("<h3>").text(tag.name);
-                    var $content=$("<ul>");
-
-                    tag.toDos.forEach(function (description) {
-                        var $li=$("<li>").text(description);
-                        $content.append($li);
-                    });
-
-                    $("main .content").append($tagName);
-                    $("main .content").append($content);
-                });
-
-            } else if ($element.parent().is(":nth-child(4)")) {
-                $input = $("<input>"),
-                $button = $("<button>").text("+");
-                $button.on = n("click", function () {
-                    toDos.push($input.val());
-                    $input.val("");
-                    });
-            $content = $("<div>").append($input).append($button);
-            }
-            $("main .content").append($content);
-            return false;
-        });
-    });
-    $(".tabs a:first-child span").trigger("click");
+	var tagObjects = tags.map(function (tag) {
+		// здесь мы находим все задачи,
+		// содержащие этот тег
+		var toDosWithTag = []; 
+		toDoObjects.forEach(function (toDo) {
+			// проверка, что результат
+			// indexOf is *не* равен -1
+			if (toDo.tags.indexOf(tag) !== -1) { 
+				toDosWithTag.push(toDo.description);
+			}
+		});
+		// мы связываем каждый тег с объектом, который // содержит название тега и массив
+		return { "name": tag, "toDos": toDosWithTag };
+	});
+	console.log(tagObjects);
+	return tagObjects;
 };
-$(document).ready(main);
+
+var main = function (toDoObjects) {
+	"use strict";
+	var toDos = toDoObjects.map(function (toDo) {
+		return toDo.description;
+	});
+
+    $("document").ready(function () {
+		$(".tabs a span").toArray().forEach(function (element) {
+			$(element).on("click", function () {
+				var $element = $(element);
+				$(".tabs a span").removeClass("active");
+				$(element).addClass("active");
+				$("main .content").empty();
+    
+                if ($element.parent().is(":nth-child(1)")) {
+					for (var i = toDos.length - 1; i > -1; i--) {
+						$(".content").append($("<li>").text(toDos[i]));
+					}
+				}
+				else if ($element.parent().is(":nth-child(2)")) {
+					toDos.forEach(function (todo) {
+						$(".content").append($("<li>").text(todo));
+					});
+				}
+				else if ($element.parent().is(":nth-child(3)")) {
+					var organizedByTag = organizeByTags(toDoObjects);
+ 
+					organizedByTag.forEach(function (tag) { 
+						var $tagName = $("<h3>").text(tag.name), 
+						$content = $("<ul>"); 
+						tag.toDos.forEach(function (description) { 
+							var $li = $("<li>").text(description); 
+							$content.append($li);
+						});
+						$("main .content").append($tagName); 
+						$("main .content").append($content);
+					});
+
+				}
+
+				else if ($element.parent().is(":nth-child(4)")) { 
+					$(".content").append(
+						'<input type="text" class="inp">' +
+						'<button class="btn">Добавить</button>'
+					);
+					var newToDo;
+					$('.btn').on('click', function () {
+						newToDo = $('.inp').val();
+						if (newToDo != '') {
+							toDos.push(newToDo);
+							alert('Новое задание "' + newToDo + '" успешно добавлено!');
+							$('.inp').val("");
+						}
+					})
+				}
+				return false;
+			})
+        })
+        $(".tabs a:first-child span").trigger("click");
+    })
+};
 $(document).ready(function () {
     $.getJSON("todos.json", function (toDoObjects) {
         main(toDoObjects);
     });
 });
-
-    
